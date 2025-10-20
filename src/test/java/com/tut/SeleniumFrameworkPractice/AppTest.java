9902aba9991bc48019c65a99b3c4c02caab3d83c
Mayuresh <mayureshpatole777@gmail.com>
2025-10-20 20:23:13 +0530

    Updated Code for CICD

--- src/test/java/com/tut/SeleniumFrameworkPractice/AppTest.java
+++ src/test/java/com/tut/SeleniumFrameworkPractice/AppTest.java
@@ -1,38 +1,31 @@
 package com.tut.SeleniumFrameworkPractice;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.testng.annotations.Test;
+import java.time.Duration;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+public class AppTest
 {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void TestMain() {
+	System.out.println("Hello World!");
+    WebDriver driver=new ChromeDriver();
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+    WebElement sel=driver.findElement(By.xpath("//select[@id='dropdown-class-example']"));
+    Select s=new Select(sel);
+    s.selectByValue("option1");
+    driver.findElement(By.id("name")).sendKeys("MayureshPatole");
+    driver.findElement(By.id("alertbtn")).click();
+    driver.quit();
+}
 }
---
+++ src/test/java/com/tut/SeleniumFrameworkPractice/New_test.java
@@ -0,0 +1,29 @@
+package com.tut.SeleniumFrameworkPractice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class New_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println( "Hello World!" );
+        WebDriver driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        WebElement sel=driver.findElement(By.xpath("//select[@id='dropdown-class-example']"));
+        Select s=new Select(sel);
+        s.selectByValue("option1");
+        driver.findElement(By.id("name")).sendKeys("MayureshPatole");
+        driver.findElement(By.id("alertbtn")).click();
+        driver.quit();
+		
+	}
+
+}
